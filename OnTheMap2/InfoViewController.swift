e465dc357389821d4d084d25c6b96ce53dfeb04b
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-07 21:36:37 -0500

    Edits

--- OnTheMap2/InfoViewController.swift
+++ OnTheMap2/InfoViewController.swift
@@ -6,4 +6,71 @@
 //  Copyright © 2017 Deborah. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreLocation
+import MapKit
+
+class InfoViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet var onthemapButton: UIButton!
+    @IBOutlet var mapinfoView: MKMapView!
+    @IBOutlet var cancelButton: UIButton!
+    @IBOutlet var linktext: UITextView!
+    @IBOutlet var locationtext: UITextView!
+    @IBOutlet var submitButton: UIButton!
+    @IBOutlet var label: UILabel!
+    
+    let geoCodeError = "Cannot Geocode!"
+    
+    var activityIndicator: UIActivityIndicatorView?
+    var cords : CLPlacemark?
+    
+    override func viewDidLoad() {
+        
+    linktext.delegate = self
+    self.locationtext.delegate = self
+    self.linktext.alpha = 0
+    self.submitButton.isHidden = true
+    tapOutKeyboard()
+    linktext.returnKeyType = .done
+    locationtext.returnKeyType = .done
+        
+    }
+    
+    fileprivate func viewMap() {
+    UIView.animate(withDuration: 0.5, animations: {
+    self.label.isHidden = true
+    self.locationtext.alpha = 0.0
+    self.onthemapButton.alpha = 0
+    self.submitButton.isHidden = false
+    self.onthemapButton.isEnabled = false
+    self.linktext.alpha = 1
+    self.view.backgroundColor = self.locationtext.backgroundColor
+    self.cancelButton.titleLabel?.textColor = UIColor.white
+    })
+        
+    let place = MKPlacemark(placemark: self.cords!)
+    self.mapinfoView.addAnnotation(place)
+        
+    let region = MKCoordinateRegionMakeWithDistance((place.location?.coordinate)!, 5000.0, 7000.0)
+        
+    mapinfoView.setRegion(region, animated: true)
+        
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+    if text == "\n" {
+    dismissKeyboard()
+    return false
+    }
+    return true
+    }
+        
+    func textViewDidEndEditing(_ textView: UITextView) {
+    dismissKeyboard()
+    }
+        
+    func textViewDidBeginEditing(_ textView: UITextView) {
+    textView.text = nil
+    }
+    }
+    }
+
+
--- OnTheMap2/StudentInfoController.swift
+++ OnTheMap2/StudentInfoController.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+let StudentInfoUpdated = "StudentInfoUpdated"
+
+    struct StudentInfoController {
+    
+    static let createdAtKey = "createdAt"
+    static let firstNameKey = "firstName"
+    static let lastNameKey = "lastName"
+    static let latitudeKey = "latitude"
+    static let longitudeKey = "longitude"
+    static let mapStringKey = "mapString"
+    static let mediaURLKey = "mediaURL"
+    static let objectIdKey = "objectId"
+    static let uniqueKeyKey = "uniqueKey"
+    static let updatedAtKey = "updatedAt"
+    static var people = [StudentInfo]()
+    
+    static func studentList(withStudents newStudents: [[String: AnyObject]]) {
+    people.removeAll()
+        
+    for firststudent in newStudents {
+    let newStudent = StudentInfo(peopleInfo: firststudent)
+    StudentInfoController.people.append(newStudent)
+            
+    }
+        
+    let note = Notification(name: Notification.Name(rawValue: StudentInfoUpdated), object: nil)
+    NotificationCenter.default.post(note)
+        
+    }
+    }
