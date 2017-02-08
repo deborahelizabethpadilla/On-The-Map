489cab44fdacb8216f57aa28975c790b0f040906
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-07 22:50:36 -0500

    Edits

--- OnTheMap2/LoginViewController.swift
+++ OnTheMap2/LoginViewController.swift
@@ -17,24 +17,70 @@ class LoginViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     
     @IBAction func signupButton(_ sender: Any) {
-    
-    }
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-       
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+        
     }
 
     @IBAction func loginButton(_ sender: Any) {
     
     }
+    
+    let spinner = showSpinner()
+    UdacityAPI.signInWithLogin(username, password: password) { (data, response, error) in
+    spinner.hide()
+    
+    if let response = response as? HTTPURLResponse {
+    if response.statusCode < 200 || response.statusCode > 300 {
+    self.presentAlert("Oops!", message: "Try Again Later!", actionTitle: "Return")
+    return
+    
+    }
+    
+    if let error = error {
+    if error.code == NSURLErrorNotConnectedToInternet {
+    
+    let alertViewMessage = self.invalidLinkMessage
+    let okActionAlertTitle = "OK"
+    
+    self.presentAlert("Not Working!", message: alertViewMessage, actionTitle: okActionAlertTitle, actionHandler: nil)
+    }
+    
+    } else {
+    do {
+    if let json = try JSONSerialization.jsonObject(with: data!, options: [.allowFragments]) as? [String:AnyObject] {
+    if let accountDict = json["account"] as? [String:AnyObject] {
+    User.uniqueKey = accountDict["key"] as! String
+    DispatchQueue.main.async(execute: {
+    
+    if let tabBarVC = self.storyboard?.instantiateViewController(withIdentifier: "TabBarViewController") {
+    self.present(tabBarVC, animated: true, completion: nil)
+    }
+    })
+    } else {
+    
+    self.presentAlert("Incorrect Login", message: "Username/Password Incorect", actionTitle: "OK")
+    }
+    }
+    
+    } catch {
+    
+    }
+    }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    dismissKeyboard()
+    return false
+    }
+    func tapOutKeyboard() {
+    let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:#selector(LoginViewController.dismissKeyboard))
+    view.addGestureRecognizer(tap)
+    }
+    
     }
 
-
+    }
--- OnTheMap2/TableViewController.swift
+++ OnTheMap2/TableViewController.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2017 Deborah. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var tableView: UITableView!
+    
+    let reuseIdentifier = "infoCell"
+    
+    let returnActionTitle = "Return"
+    let invalidLinkProvidedMessage = "Oh No! Can't Open Link!"
+    let badLinkTitle = "Oh No! Bad URL!"
+    let parseRetrievalFailedTitle = "No Data!"
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.delegate = self
+    tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    let numberofPeople = StudentInfoController.people.count
+    return numberofPeople
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for:  indexPath)
+    let studentInfo = StudentInfoController.people[indexPath.row]
+    cell.textLabel?.text = "\(studentInfo.firstName) \(studentInfo.lastName)"
+    cell.detailTextLabel?.text = "\(studentInfo.mediaURL)"
+        
+    return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    let app = UIApplication.shared
+        
+    guard let providedURL = tableView.cellForRow(at: indexPath)?.detailTextLabel?.text,
+    let url = URL(string: providedURL), app.openURL(url) == true else {
+                
+    let alertViewMessage = invalidLinkProvidedMessage
+    let alertActionTitle = returnActionTitle
+                
+    presentAlert(badLinkTitle, message: alertViewMessage, actionTitle: alertActionTitle)
+    return
+                
+    }
+    }
+    }
+
