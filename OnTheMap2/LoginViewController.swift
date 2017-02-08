547ec4f20122d2305b520db681d0a0704337d65b
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-07 20:08:06 -0500

    First Commit

---
+++ OnTheMap2/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap2
+//
+//  Created by Deborah on 2/7/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
