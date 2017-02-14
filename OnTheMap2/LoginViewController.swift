784efb66b668a63930bf301a1fe69b233fa7a242
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-14 16:36:21 -0500

    Login VC

--- OnTheMap2/LoginViewController.swift
+++ OnTheMap2/LoginViewController.swift
@@ -76,10 +76,14 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     UdacityAPI.signInWithLogin(usernameField.text!, password: passwordField.text!) { (data, user, error) in
     spinner.hide()
     if user != nil {
+        
     //Logged In!
+        
+    self.performSegue(withIdentifier: "login", sender: self)
     if let accountDict = ["account"] as? [String:AnyObject] {
     Users.uniqueKey = accountDict["key"] as! String
     DispatchQueue.main.async(execute: {
+        
     //Present The Map And Tabbed View
     if let tabBarVC = self.storyboard?.instantiateViewController(withIdentifier: "TabBarViewController") {
     self.present(tabBarVC, animated: true, completion: nil)
