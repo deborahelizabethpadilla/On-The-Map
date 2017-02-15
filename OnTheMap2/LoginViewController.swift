920daae828b5813f3ba947577d6d0536fca7396e
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-15 15:14:12 -0500

    Login VC

--- OnTheMap2/LoginViewController.swift
+++ OnTheMap2/LoginViewController.swift
@@ -46,6 +46,16 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         passwordField.delegate = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
     @IBAction func signupButton(_ sender: Any) {
         _ = self.storyboard?.instantiateViewController(withIdentifier: "SignupViewController")
         UIApplication.shared.openURL(URL(string: url.URL)!)
@@ -77,31 +87,27 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         UdacityAPI.signInWithLogin(usernameField.text!, password: passwordField.text!) { user, response, error in
             spinner.hide()
             if user != nil {
-            
+                
                 //Logged In!
+                //Present The Map And Tabbed View
                 
                 self.performSegue(withIdentifier: "login", sender: self)
-               
                 
-                //Present The Map And Tabbed View
-                if let tabBarVC = self.storyboard?.instantiateViewController(withIdentifier: "TabBarViewController") {
-                    self.present(tabBarVC, animated: true, completion: nil)
-                return
-                }
-            
-            
+                
+                
+                
             } else {
                 
                 self.displayAlert(title: "Failed Log In!", message: "Try Again!")
-    
+                
             }
-        
+            
             if let response = user as? HTTPURLResponse {
                 if response.statusCode < 200 || response.statusCode > 300 {
                     self.displayAlert(title: "Try Again Later!", message: "Error!")
                     return
                 }
-            
+                
             }
             if let error = error {
                 //Network Error
@@ -116,6 +122,31 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             }
         }
     }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillHide(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWillShow(_ notification: Notification) {
+        self.view.frame.origin.y = -getKeyboardHeight(notification)
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        self.view.frame.origin.y = 0
+    }
+    
 }
 
 
